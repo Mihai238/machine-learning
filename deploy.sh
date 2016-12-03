7edb21c2d8dbca83486fbfb245e2a6f7de8f9f56
Mihai Lepadat <mihailepadat@gmail.com>
2016-12-03 12:33:09 +0100

    download spark if not available

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,13 @@
 #! /bin/sh
 
+if [ ! -d "spark" ];
+then
+    FILE_NAME=spark-2.0.2-bin-hadoop2.7
+    URL=http://d3kbcqa49mib13.cloudfront.net/spark-2.0.2-bin-hadoop2.7.tgz
+    curl ${URL} | tar xz
+    mv ${FILE_NAME} spark
+fi
+
 sbt package
-cp -f target/scala-2.11/machine-learning_2.11-1.0.jar ~/apps/spark-2.0.2-bin-hadoop2.7
+
+./spark/bin/spark-submit --class "App" target/scala-2.11/machine-learning_2.11-1.0.jar
